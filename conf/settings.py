df936c0fb2475f602af1ec4c5973e6c704e31c28
Hugo Cachitas <hugo.cachitas@research.fchampalimaud.org>
2019-05-15 19:33:11 +0100

    Set DEFAULT_PAGINATION_CLASS

--- conf/settings.py
+++ conf/settings.py
@@ -201,6 +201,7 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.BasicAuthentication",
         "rest_framework.authentication.SessionAuthentication",
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     "PAGE_SIZE": 50,
     "DEFAULT_RENDERER_CLASSES": (
         "rest_framework.renderers.JSONRenderer",
