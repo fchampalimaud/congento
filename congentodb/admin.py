728bc13fc41ec4488ca480a78b64c1a6c71787bb
Hugo Cachitas <hugo.cachitas@research.fchampalimaud.org>
2019-05-15 17:41:13 +0100

    Register missing models in admin app

--- congentodb/admin.py
+++ congentodb/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import Institution, Rodent
+from .models import Institution
+from .models import Rodent
+from .models import Fly
+from .models import Zebrafish
 
 admin.site.register(Institution)
 admin.site.register(Rodent)
+admin.site.register(Fly)
+admin.site.register(Zebrafish)
